e4070d8a95b7df5712cdf56a27e7036e37ee5594
celsisus <ezrasussman@gmail.com>
2021-05-19 12:39:53 -0400

    custom creeper meat class that explodes on consumption

---
+++ src/main/java/com/ejs/mobmeater/registry/CreeperMeatItem.java
@@ -0,0 +1,52 @@
+package com.ejs.mobmeater.registry;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
+
+public class CreeperMeatItem extends Item {
+
+	public CreeperMeatItem(Item.Settings settings) {
+		super(settings);
+	}
+	
+	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+		ItemStack itemStack = user.getStackInHand(hand);
+		user.setCurrentHand(hand);
+		if (!(world instanceof ServerWorld)) {
+			return TypedActionResult.success(itemStack);
+		} else {
+			if (!world.isClient) {
+				CompletableFuture.delayedExecutor(1, TimeUnit.SECONDS).execute(() -> {
+					user.world.createExplosion(user, user.getX(), user.getY(), user.getZ(), 4.0F, Explosion.DestructionType.BREAK);
+					user.onDeath(DamageSource.explosion(user));
+					user.setHealth(0);
+				//	user.kill();
+					world.playSoundFromEntity(user, user, SoundEvents.ENTITY_TNT_PRIMED, SoundCategory.BLOCKS, 1.0F, 1.0F);
+				});
+			}
+			return TypedActionResult.consume(itemStack);
+		}
+	}
+
+	public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+		return user.eatFood(world, stack);
+	}
+
+	public UseAction getUseAction(ItemStack stack) {
+		return UseAction.EAT;
+	}
+}
---
+++ src/main/java/com/ejs/mobmeater/registry/Loot.java
@@ -0,0 +1,55 @@
+package com.ejs.mobmeater.registry;
+
+import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.minecraft.item.Item;
+import net.minecraft.loot.UniformLootTableRange;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.util.Identifier;
+
+public class Loot {
+	private static final Identifier BLAZE_LOOT = new Identifier("minecraft", "entities/blaze");
+	private static final Identifier ENDER_LOOT = new Identifier("minecraft", "entities/enderman");
+	private static final Identifier ELDER_LOOT = new Identifier("minecraft", "entities/elder_guardian");
+	private static final Identifier GHAST_LOOT = new Identifier("minecraft", "entities/ghast");
+	private static final Identifier GUARDIAN_LOOT = new Identifier("minecraft", "entities/guardian");
+	private static final Identifier MAGMA_LOOT = new Identifier("minecraft", "entities/magma");
+	private static final Identifier RAVAGER_LOOT = new Identifier("minecraft", "entities/ravager");
+	private static final Identifier SKELE_LOOT = new Identifier("minecraft", "entities/skeleton");
+	private static final Identifier SLIME_LOOT = new Identifier("minecraft", "entities/slime");
+	private static final Identifier CREEPER_LOOT = new Identifier("minecraft", "entities/creeper");
+	
+	private static final Identifier VILLAGER = new Identifier("minecraft", "entities/villager");
+	private static final Identifier PILLAGER = new Identifier("minecraft", "entities/pillager");
+	private static final Identifier EVOKER = new Identifier("minecraft", "entities/evoker");
+	private static final Identifier VINDICATOR = new Identifier("minecraft", "entities/vindicator");
+	private static final Identifier WITCH = new Identifier("minecraft", "entities/witch");
+
+	private static final Identifier[] LOOT_TABLES = { BLAZE_LOOT, ENDER_LOOT, ELDER_LOOT, GHAST_LOOT, GUARDIAN_LOOT,
+			MAGMA_LOOT, RAVAGER_LOOT, SKELE_LOOT, SLIME_LOOT, CREEPER_LOOT };
+	private static final Item[] DROPS = { ModItems.BLAZE_MEAT, ModItems.ENDER_MEAT, ModItems.ELDER_MEAT,
+			ModItems.GHAST_MEAT, ModItems.GUARDIAN_MEAT, ModItems.MAGMA_MEAT, ModItems.RAVAGER_MEAT,
+			ModItems.BONE_MARROW, ModItems.SLIME_MEAT, ModItems.CREEPER_MEAT };
+
+	public static void init() {
+		for (int i = 0; i < LOOT_TABLES.length; i++) {
+			final int j = i;
+			LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, supplier, setter) -> {
+				if (LOOT_TABLES[j].equals(id)) {
+					FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+							.rolls(UniformLootTableRange.between(1, 5)).withEntry(ItemEntry.builder(DROPS[j]).build());
+
+					supplier.pool(poolBuilder);
+				}
+			});
+		}
+		LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, supplier, setter) -> {
+			if (VILLAGER.equals(id)||PILLAGER.equals(id)||EVOKER.equals(id)||VINDICATOR.equals(id)||WITCH.equals(id)) {
+				FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+						.rolls(UniformLootTableRange.between(1, 5)).withEntry(ItemEntry.builder(ModItems.FLESH).build());
+
+				supplier.pool(poolBuilder);
+			}
+		});
+	}
+}
